57f65d09303326710ffa9a029ebbdc57057d4ab2
pedigru3 <contosdeferreira@hotmail.com>
2023-12-13 10:24:17 -0300

    trying fix hour diff 2

--- src/app/api/v1/availability/table/route.ts
+++ src/app/api/v1/availability/table/route.ts
@@ -25,9 +25,11 @@ export async function GET(request: NextRequest) {
       return Response.json({ error: 'Hour not provided.' }, { status: 400 })
     }
 
-    const referenceDate = dayjs(String(dateParam))
+    const referenceDate = dayjs
+      .utc(String(dateParam))
       .set('hour', Number(hourParam))
-      .tz('America/Sao_Paulo')
+
+    console.log('DATA ATUAL: ', referenceDate.format())
 
     // Check if the reference date is in the past; if so, return a response indicating an old date
     const isPastDate = referenceDate.endOf('day').isBefore(new Date())
