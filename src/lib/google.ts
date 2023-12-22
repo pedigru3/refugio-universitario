23e3fd11007590713e7148e4b4225fd91428a8e8
pedigru3 <contosdeferreira@hotmail.com>
2023-12-22 13:52:57 -0300

    google fix after

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -16,6 +16,8 @@ export async function getGoogleOAuthToken(userId: string) {
     process.env.GOOGLE_CLIENT_SECRET,
   )
 
+  console.log('access_token_expires: ', account.access_token_expires)
+
   const expiryDate = account.access_token_expires
     ? new Date().getTime() - new Date(account.access_token_expires).getTime()
     : 1000 * 60 * 60
@@ -30,7 +32,8 @@ export async function getGoogleOAuthToken(userId: string) {
     return auth
   }
 
-  const isTokenExpired = dayjs(expiryDate).isBefore(new Date())
+  const isTokenExpired = dayjs(expiryDate).isAfter(new Date())
+  console.log('token expired: ', isTokenExpired)
 
   if (isTokenExpired) {
     const { credentials } = await auth.refreshAccessToken()
